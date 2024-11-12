e45923fd5a422f5d60065e4cc2406b7e3b3aae25
Karen Arai <ittarikitari.arai@gmail.com>
2024-11-13 02:29:53 +0900

    #8　ホームページ　追加

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+</script>
+
+<template>
+  <div class="homeContainer">
+  </div>
+</template>
+
+<style scoped>
+.homeContainer {
+  padding: var(--m) 0;
+  display: flex;
+  flex-direction: column;
+  gap: var(--m);
+}
+</style>
