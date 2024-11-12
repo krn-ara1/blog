42d826dc12678b41bfdeda881c9e9cd3e513bc76
Karen Arai <ittarikitari.arai@gmail.com>
2024-11-12 19:58:37 +0900

    #10　デフォルトテンプレート　追加

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<script setup>
+</script>
+
+<template>
+  <div class="container">
+    <div class="wrapper">
+      <div class="mainAndPageTop">
+        <main class="main">
+          <slot />
+        </main>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+}
+.wrapper {
+  min-height: 100vh;
+  padding-top: calc(
+    (0.8rem * 1.6 + 0.8rem * 1.6 * 0.2 * 2) * 2 + (0.8rem * 0.5) + (0.8rem * 1) *
+      2
+  );
+  display: flex;
+  flex-direction: column;
+}
+.mainAndPageTop {
+  flex-grow: 1;
+}
+.main {
+  width: 100%;
+  overflow-x: hidden;
+  font-size: 1rem;
+}
+</style>
