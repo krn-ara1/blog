2d3e38a39853657dcbd63ec83a1c694399c1de7f
Karen Arai <ittarikitari.arai@gmail.com>
2024-11-13 13:07:15 +0900

    #7　ページトップコンポーネント　リファクタリング

--- components/pagetop/Pagetop.vue
+++ components/pagetop/Pagetop.vue
@@ -105,7 +105,7 @@ const scrollPageTop = () => {
     opacity: 1;
   }
 }
-@media screen and (max-width: 1280px) {
+@media not screen and (min-width: 1280px) {
   .pagetopButton {
     transform: translateX(0);
   }
