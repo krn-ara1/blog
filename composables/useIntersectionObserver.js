9bf357e7fdc1e145d3b74a5ed34a8cd2ba56017b
Karen Arai <ittarikitari.arai@gmail.com>
2024-11-20 00:40:03 +0900

    #2　環境構築　要素交差時の処理を追加

---
+++ composables/useIntersectionObserver.js
@@ -0,0 +1,34 @@
+const doObserve = (elements) => {
+  const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.1,
+  };
+  elements.forEach((element, index) => {
+    const observer = new IntersectionObserver((items) => {
+      items.forEach((item) => {
+        if (item.isIntersecting && item.target.classList.contains("-delay")) {
+          const delay = 300 * index;
+          setTimeout(() => {
+            item.target.classList.add("-intersecting");
+          }, delay);
+        } else if (item.isIntersecting) {
+          item.target.classList.add("-intersecting");
+        } else {
+          item.target.classList.remove("-intersecting");
+        }
+
+        if (item.isIntersecting && item.target.classList.contains("-once")) {
+          observer.unobserve(element.value);
+        }
+      });
+    }, options);
+    observer.observe(element.value);
+  });
+};
+
+export const useIntersectionObserver = () => {
+  return {
+    doObserve,
+  };
+};
